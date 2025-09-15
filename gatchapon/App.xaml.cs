e162bb87eb569884e345d6348e6d9c2bf56e1ee2
joachim <joachimleongson@gmail.com>
2025-09-16 00:30:10 +0800

    Reapply "Added .gitignore"
    This reverts commit 6b84bf6298d092e6d20656021c5ae88db644fa41.

---
+++ gatchapon/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace gatchapon
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
