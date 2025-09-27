d8698466700b3e1da324d2ea616c27d8333f1a8f
joachim <joachimleongson@gmail.com>
2025-09-27 19:42:16 +0800

    Register

---
+++ gatchapon/Register.xaml.cs
@@ -0,0 +1,15 @@
+namespace gatchapon
+{
+    public partial class Register : ContentPage
+    {
+        public Register()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object? sender, EventArgs e)
+        {
+
+        }
+    }
+}
