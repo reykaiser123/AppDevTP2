c3da4d98cd88081a228871bbcb79656fd2b848de
joachim <joachimleongson@gmail.com>
2025-09-28 14:50:18 +0800

    Shop.xaml.cs

---
+++ gatchapon/Shop.xaml.cs
@@ -0,0 +1,14 @@
+namespace gatchapon 
+{
+    public partial class Shop : ContentPage
+    {
+        public Shop()
+        {
+            InitializeComponent();
+        }
+        private void OnCounterClicked(object? sender, EventArgs e)
+        {
+
+        }
+    }
+}
