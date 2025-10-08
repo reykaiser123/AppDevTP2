f8d20bf9188f16e15184ad32226794842033b8d4
Oswell Genesis Dingson <oswell2022@gmail.com>
2025-10-08 09:25:09 +0800

    new changes dashboard

--- gatchapon/Dashboard.xaml.cs
+++ gatchapon/Dashboard.xaml.cs
@@ -7,4 +7,9 @@ public partial class Dashboard : ContentPage
 		InitializeComponent();
 		NavigationPage.SetHasBackButton(this, false);
     }
+    private async void OnProfileClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ProfileSetting()); 
+    }
+
 }
