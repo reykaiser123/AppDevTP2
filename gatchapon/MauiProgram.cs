5538250aaacb89d7e83646d1c58788ae2e57a14f
Oswell Genesis Dingson <oswell2022@gmail.com>
2025-11-04 09:16:59 +0800

    333

--- gatchapon/MauiProgram.cs
+++ gatchapon/MauiProgram.cs
@@ -1,6 +1,7 @@
 ﻿using Firebase.Auth;
 using Firebase.Auth.Providers;
 using Microsoft.Extensions.Logging;
+using CommunityToolkit.Maui;
 
 namespace gatchapon
 {
@@ -11,6 +12,7 @@ namespace gatchapon
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseMauiCommunityToolkit() // ✅ Enables popups and other toolkit features
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
@@ -19,10 +21,11 @@ namespace gatchapon
 
 #if DEBUG
             builder.Logging.AddDebug();
-    		builder.Logging.AddDebug();
 
+            // Dependency registration
             builder.Services.AddTransient<Login>();
 
+            // Firebase Authentication setup
             builder.Services.AddSingleton(new FirebaseAuthClient(new FirebaseAuthConfig()
             {
                 ApiKey = "AIzaSyD1bX4k0bY3aX2n8KXG6ZyW8g5v7mJHh6M",
